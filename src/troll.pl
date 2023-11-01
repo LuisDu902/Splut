6f0a1e512740295e49143026da8dc85b50881cb3
Luis Du <luisdu.s23@gmail.com>
2023-11-01 15:41:52 +0000

    removing hit pieces

--- src/troll.pl
+++ src/troll.pl
@@ -6,6 +6,14 @@ valid_troll_move(Pos, Direction):-
     new_pos(Pos, Direction, NewPos),
     ((position(r-_, NewPos); \+ position(_-_, NewPos)) -> true ; false).
 
+valid_throw_direction(Board, Player, Position, Direction) :-
+    length(Board, Size),
+    new_pos(Position, Direction, NewPos),
+    inside_board(NewPos, Size),
+    next_player(Player, P),
+    \+ position(r-_, NewPos),
+    \+ position(t-P, NewPos).
+
 % ----------------------------------------
 
 pull_rock_option(Option):-
@@ -40,15 +48,6 @@ throw_rock_option(Board, T-Player, Position, Direction):-
         fail
     ).
 
-valid_throw_direction(Board, Player, Position, Direction) :-
-    length(Board, Size),
-    new_pos(Position, Direction, NewPos),
-    inside_board(NewPos, Size),
-    next_player(Player, P),
-    \+ position(r-_, NewPos),
-    \+ position(t-P, NewPos).
-
-
 throw_rock(Board, Pos, Troll, Dir, ThrowDir, NewBoard) :-
     new_pos(Pos, Dir, NewPos),
     position(Rock, NewPos),
@@ -73,6 +72,8 @@ troll_move(Board, Troll, Pos, Direction, NewBoard) :-
         general_move(Board, Troll, Pos, NewPos, NewBoard)
     )).
 
+% ----------------------------------------
+
 move_rock(Board, Rock, Direction, NewBoard):-
     position(Rock, X-Y),
     length(Board, Size),
@@ -80,34 +81,35 @@ move_rock(Board, Rock, Direction, NewBoard):-
       nth1(Y, Board, List), R is Size - X, get_remaining(X, List, Size, Rest, Direction)),
     new_rock_pos(Rest, X-Y, Direction, NewX-NewY),
     format('THE ROCK SHOULD GO FROM (~d,~d) TO (~d,~d)\n', [X, Y, NewX, NewY]),
+    (position(Piece, NewX-NewY) -> retract(position(Piece, NewX-NewY)); true),
     update_piece_pos(Rock, NewX-NewY), !,
     put_piece(Board, Rock, NewX-NewY, NewBoard).
-
+    
 new_rock_pos([], Pos, _, Pos).
 
 % move up
-new_rock_pos(List, X-Y, 1, NewX-NewY):-
-    find_reverse_elem(List, [r-_, t-_, e-e], A),
-    NewX is X,
-    NewY is Y - A.
+new_rock_pos(List, X-Y, 1, X-NewY):-
+    find_reverse_elem(List, [r-_, t-_, e-e, s-_], A),
+    TmpY is Y - A - 1,
+    (position(s-_, X-TmpY) -> NewY is TmpY; NewY is Y - A).
 
 % move down
-new_rock_pos(List, X-Y, 2, NewX-NewY):-
-    find_elem(List, [r-_, t-_, e-e], A),
-    NewX is X,
-    NewY is Y + A.
+new_rock_pos(List, X-Y, 2, X-NewY):-
+    find_elem(List, [r-_, t-_, e-e, s-_], A),
+    TmpY is Y + A + 1,
+    (position(s-_, X-TmpY) -> NewY is TmpY; NewY is Y + A).
 
 % move left
-new_rock_pos(List, X-Y, 3, NewX-NewY):-
-    find_reverse_elem(List, [r-_, t-_, e-e], A),
-    NewX is X - A,
-    NewY is Y.
+new_rock_pos(List, X-Y, 3, NewX-Y):-
+    find_reverse_elem(List, [r-_, t-_, e-e, s-_], A),
+    TmpX is X - A - 1,
+    (position(s-_, TmpX-Y) -> NewX is TmpX; NewX is X - A).
 
 % move right
-new_rock_pos(List, X-Y, 4, NewX-NewY):-
+new_rock_pos(List, X-Y, 4, NewX-Y):-
     find_elem(List, [r-_, t-_, e-e], A),
-    NewX is X + A,
-    NewY is Y.
+    TmpX is X + A + 1,
+    (position(s-_, TmpX-Y) -> NewX is TmpX; NewX is X + A).
 
 get_remaining(N, List, Size, Rest, Direction):-
     format('DIRECTION IS ~d\n', [Direction]),
